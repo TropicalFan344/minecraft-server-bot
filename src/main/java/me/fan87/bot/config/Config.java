97f291e83e9f8b2ced77d1c539711f550c6023ac
tropicalfan344 <cytusiilover0625@gmail.com>
2023-06-03 17:09:21 +0800

    my token!!

--- src/main/java/me/fan87/bot/config/Config.java
+++ src/main/java/me/fan87/bot/config/Config.java
@@ -2,7 +2,7 @@ package me.fan87.bot.config;
 
 public class Config {
 
-    public String token = "MTExMjczMjc4MzAyMjk2ODkxMg.GdeHkb.PYp7eFK_5e58uWSJYkGT3PSwdfopgUZudoFhjE";
-    public String serverUri = "C:\\Users\\moon0\\Desktop\\1.8.9 server";
+    public String token = "input your token here";
+    public String serverUri = "server Uri here";
 
 }
